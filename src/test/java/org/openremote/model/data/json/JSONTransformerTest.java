4fe0f3a1f0fc8abd3ff757990dcb9d6c116a3037
Juha Lindfors <juha@openremote.org>
2014-09-21 19:30:36 +0200

    Add tests for JSONTransformer string validators.

--- src/test/java/org/openremote/model/data/json/JSONTransformerTest.java
+++ src/test/java/org/openremote/model/data/json/JSONTransformerTest.java
@@ -21,6 +21,7 @@
 package org.openremote.model.data.json;
 
 import org.openremote.base.exception.IncorrectImplementationException;
+import org.openremote.model.Model;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -51,6 +52,113 @@ public class JSONTransformerTest
       // expected...
     }
   }
+
+
+
+  // Class StringValidator Tests ------------------------------------------------------------------
+
+  /**
+   * Test that default validator implementation for JSON strings gets initialized.
+   */
+  @Test public void testDefaultStringValidatorInitialization()
+  {
+    Assert.assertTrue(JSONTransformer.getStringValidator() == JSONTransformer.defaultStringValidator);
+  }
+
+
+  /**
+   * Test default JSON string validator implementation behavior on null strings.
+   *
+   * @throws Exception    if test fails
+   */
+  @Test (expectedExceptions = Model.ValidationException.class)
+
+  public void testDefaultStringValidatorImplementationNullString() throws Exception
+  {
+    JSONTransformer.getStringValidator().validate(null);
+  }
+
+
+  /**
+   * Test string of length one on default JSON string validator implementation. Should pass...
+   *
+   * @throws Exception    if test fails
+   */
+  @Test public void testDefaultStringValidatorImplementation() throws Exception
+  {
+    JSONTransformer.getStringValidator().validate("a");
+  }
+
+  /**
+   * Test default JSON string validator implementation against upper limit defined in
+   * {@link Model#DEFAULT_STRING_ATTRIBUTE_LENGTH_CONSTRAINT}
+   *
+   * @throws Exception    if test fails
+   */
+  @Test public void testDefaultStringValidatorImplementationUpperLimit() throws Exception
+  {
+    StringBuilder builder = new StringBuilder(256);
+
+    // Test upper limit...
+
+    for (int index = 0; index < Model.DEFAULT_STRING_ATTRIBUTE_LENGTH_CONSTRAINT; ++index)
+    {
+      builder.append("a");
+    }
+
+    JSONTransformer.getStringValidator().validate(builder.toString());
+
+
+    // Test the boundary one above upper limit...
+
+    builder.append("a");
+
+    try
+    {
+      JSONTransformer.getStringValidator().validate(builder.toString());
+
+      Assert.fail("should not get here..");
+    }
+
+    catch (Model.ValidationException exception)
+    {
+      // expected...
+    }
+  }
+
+
+  /**
+   * Test changing the default JSON string validator a custom impl. that restricts string
+   * lengths to 8 characters...
+   *
+   * @throws Exception      if test fails
+   */
+  @Test (expectedExceptions = Model.ValidationException.class)
+
+  public void testCustomStringValidator() throws Exception
+  {
+    try
+    {
+      JSONTransformer.setStringValidator(new JSONTransformer.JSONValidator<String>()
+      {
+        @Override public void validate(String attribute) throws Model.ValidationException
+        {
+          if (attribute == null || attribute.length() > 8)
+          {
+            throw new Model.ValidationException("error");
+          }
+        }
+      });
+
+      JSONTransformer.getStringValidator().validate(("123456789"));
+    }
+
+    finally
+    {
+      JSONTransformer.setStringValidator(JSONTransformer.defaultStringValidator);
+    }
+  }
+
 
   // Nested Classes -------------------------------------------------------------------------------
 
